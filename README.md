39c38b9ec1f9333a59b580848e0c6ced9f0c85f1
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2025-03-04 19:13:07 -0800

    Implement a basic working to-do
    1. Ability to insert a to-do item
    2. Ability to mark an item as done, using ID
    3. Ability to list all items, whether done or not done

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # clisty
 CLI based To-do list manager
 
+Right now, this has the efficiency of using a fork to drink a soup. It works, but not quite right.
+
 ## References
 1. https://pkg.go.dev/flag
 2. https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
--- clisty.go
+++ clisty.go
@@ -1,50 +1,121 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"slices"
+	"strconv"
 )
+
+const (
+	Add                  = "add"
+	List                 = "list"
+	Done                 = "done"
+	DefaultStoreFilePath = "store.json"
+	Capacity             = 10000
+)
+
+var config Configuration
+var store Store
+
+type Configuration struct {
+	StoreFilePath string
+}
 
 type Item struct {
 	Id   uint64   `json:"id"`
 	Name string   `json:"name"`
+	Done bool     `json:"done"`
 	Tags []string `json:"tags"`
 }
 
-const (
-	Add  = "add"
-	List = "list"
-	Done = "done"
-)
+type Store struct {
+	Capacity        uint64 `json:"capacity"`
+	TotalLength     uint64 `json:"totalLength"`
+	CompletedLength uint64 `json:"completedLength"`
+	Items           []Item `json:"items"`
+}
+
+func (s *Store) addItem(item Item) {
+	fmt.Println(s.Items)
+	if s.Capacity < s.TotalLength+1 {
+		fmt.Println("The store is already at maximum capacity")
+		return
+	}
+
+	if len(s.Items) == 0 {
+		s.Items = []Item{}
+	}
+	item.Id = s.TotalLength + 1
+	s.Items = append(s.Items, item)
+	s.TotalLength = s.TotalLength + 1
+	s.listCompletedItems()
+}
+
+func (s *Store) listCompletedItems() {
+	s.listItems(false)
+}
+
+func (s *Store) listItems(listCompleted bool) {
+	for _, item := range s.Items {
+		if !item.Done {
+			fmt.Println("[ ]", item.Id, item.Name)
+		}
+
+		if item.Done && listCompleted {
+			fmt.Println("[X]", item.Id, item.Name)
+		}
+	}
+}
+
+func (s *Store) markDone(item Item) {
+	idx := slices.IndexFunc(s.Items, func(i Item) bool { return i.Id == item.Id })
+	s.Items[idx].Done = true
+	s.CompletedLength = s.CompletedLength + 1
+	s.listCompletedItems()
+}
 
 func parseAddArgs(args []string) Item {
-	fmt.Println(args)
-	return Item{}
-}
-
-func addItem(item Item, items []Item) {
-	fmt.Println(item)
-	fmt.Println(items)
-	fmt.Println("added")
-}
-
-func listItems(items []Item) {
-	fmt.Println(items)
+	return Item{
+		Name: args[0],
+		Done: false,
+	}
 }
 
 func parseDoneArgs(args []string) Item {
-	fmt.Println(args)
-	return Item{}
+	id, err := strconv.ParseUint(args[0], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return Item{Id: id}
 }
 
-func markDone(item Item, items []Item) {
-	fmt.Println(item)
-	fmt.Println(items)
-	fmt.Println("marked done")
+func initialize(storeFilePath string) {
+	storeData, err := os.ReadFile(storeFilePath)
+	if err != nil {
+		panic(err)
+	}
+	config = Configuration{StoreFilePath: storeFilePath}
+	json.Unmarshal(storeData, &store)
+	store.Capacity = Capacity
+}
+
+func shutdown(storeFilePath string) {
+	storeData, err := json.Marshal(store)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(storeFilePath, storeData, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	initialize(DefaultStoreFilePath)
 	validCmds := []string{Add, List, Done}
 	flag.Parse()
 	args := flag.Args()
@@ -60,16 +131,15 @@ func main() {
 		return
 	}
 
-	items := []Item{}
-
 	switch args[0] {
 	case Add:
 		item := parseAddArgs(args[1:])
-		addItem(item, items)
+		store.addItem(item)
 	case List:
-		listItems(items)
+		store.listCompletedItems()
 	case Done:
 		item := parseDoneArgs(args[1:])
-		markDone(item, items)
+		store.markDone(item)
 	}
+	shutdown(DefaultStoreFilePath)
 }
